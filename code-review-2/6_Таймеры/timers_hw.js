6e29c35cb531ceb18e88904fe4b3a0b457375e70
Denis <fordz2012@gmail.com>
2024-07-22 17:33:24 +0300

    corrected task 6_timer

--- code-review-2/6_Таймеры/timers_hw.js
+++ code-review-2/6_Таймеры/timers_hw.js
@@ -1,82 +1,86 @@
 
 function getTimeForDate(endtime) {
   let end = Date.parse(endtime) - Date.parse(new Date());
-  let seconds = Math.floor((end / 1000) % 60);
-  let minutes = Math.floor((end / 1000 / 60) % 60);
-  let hours = Math.floor((end / (1000 * 60 * 60)) % 24);
-  let days = Math.floor((end / (1000 * 60 * 60 * 24)) % 30);
-  let month = Math.floor((end / (1000 * 60 * 60 * 24 * 30)));
+  const second = Math.floor(end / 1000);
+  const minute = Math.floor(second / 60);
+  const hour = Math.floor(minute / 60);
+  const day = Math.floor(hour / 24);
+  const month = Math.floor(day / 30.425);
 
   return {
-    'total': end,
-    'month': month,
-    'days': days,
-    'hours': hours,
-    'min': minutes,
-    'sec': seconds
+    'month': month % 12,
+    'days': Math.floor(day % 30.425),
+    'hours': hour % 24,
+    'min': minute % 60,
+    'sec': second % 60,
   }
 }
 
 function plurSec(num, time) {
-
-  let diff = {
+  const diff = {
     sec: ['секунд', 'секунды', 'секунда'],
     min: ['минут', 'минуты', 'минута'],
     hours: ['часов', 'часа','час'],
     days: ['дней', 'дня','день'],
     month: ['месяцев', 'месяца','месяц'],
   }
+  const remainder = num % 100;
 
-  let arrNum = [...String(num)];
-
-  if(arrNum.join('') == 1) {
-    return diff[time][2];
+  if((remainder >= 5 && remainder <= 20) || remainder % 10 === 0) {
+    return diff[time][0];
   }
-
-  if(arrNum[arrNum.length - 1] === '1' && arrNum[0] != '1' ) {
-    return diff[time][2];
+  if(remainder > 20 && remainder % 10 >= 5) {
+    return diff[time][0];
   }
-
-  if(arrNum[0] != '1' && arrNum[arrNum.length - 1] === '2' ||  arrNum[arrNum.length - 1] === '3' || arrNum[arrNum.length - 1] === '4') {
+  if(remainder % 10 === 1) {
+    return diff[time][2];
+  } else {
     return diff[time][1];
   }
-
-  return diff[time][0];
 }
 
-function renderTimer(data) {
+function renderTimer(time) {
+  const page = {
+    'month': {
+      value: document.querySelector('.timer__month > .timer__count'),
+      text: document.querySelector('.timer__month > .timer__description')
+    },
+    'days': {
+      value: document.querySelector('.timer__days > .timer__count'),
+      text: document.querySelector('.timer__days > .timer__description')
+    },
+    'hours': {
+      value: document.querySelector('.timer__hours > .timer__count'),
+      text: document.querySelector('.timer__hours > .timer__description')
+    },
+    'min': {
+      value: document.querySelector('.timer__minutes > .timer__count'),
+      text: document.querySelector('.timer__minutes > .timer__description')
+    },
+    'sec': {
+      value: document.querySelector('.timer__seconds > .timer__count'),
+      text: document.querySelector('.timer__seconds > .timer__description')
+    },
+  }
+  const data = getTimeForDate(time);
 
-  let obj = getTimeForDate(data);
-  const timerWrapper = document.querySelector('.timer__wrapper');
-
-  let timer =
-  ` <div class="timer">
-      <h2 class="timer__title">До <span>нового года</span> осталось:</h2>
-      <div class="timer__container">
-        <div class="timer__column timer__month">
-          <div class="timer__count">${obj.month < 10 ? '0' + obj.month : obj.month}</div>
-          <div class="timer__description">${plurSec(obj.month, 'month')}</div>
-        </div>
-        <div class="timer__column timer__days">
-          <div class="timer__count">${obj.days < 10 ? '0' + obj.days : obj.days}</div>
-          <div class="timer__description">${plurSec(obj.days, 'days')}</div>
-        </div>
-        <div class="timer__column timer__hours">
-          <div class="timer__count">${obj.hours < 10 ? '0' + obj.hours : obj.hours}</div>
-          <div class="timer__description">${plurSec(obj.hours, 'hours')}</div>
-        </div>
-        <div class="timer__column timer__minutes">
-          <div class="timer__count">${obj.min < 10 ? '0' + obj.min : obj.min}</div>
-          <div class="timer__description">${plurSec(obj.min, 'min')}</div>
-        </div>
-        <div class="timer__column timer__seconds">
-          <div class="timer__count">${obj.sec < 10 ? '0' + obj.sec : obj.sec}</div>
-          <div class="timer__description">${plurSec(obj.sec, 'sec')}</div>
-        </div>
-      </div>
-    </div>
-  `
-  timerWrapper.innerHTML = timer;
+  Object.entries(data).map(([key, value]) => {
+      const text = plurSec(value, key);
+      const count = data[key].toString().padStart(2, '0');
+      page[key].text.innerHTML = text;
+      page[key].value.innerHTML = count;
+  })
 }
 
-setInterval(() => {renderTimer('2025-01-01')}, 1000);
+function isNY(time) {
+  let date =`${time}:00:00:00`;
+
+  if(new Date() < new Date(date)) {
+    setInterval(() => {renderTimer(date)},1000);
+  } else {
+    clearInterval(setInterval(() => {renderTimer(date)},1000));
+    document.querySelector('.timer__header').textContent = ` ${new Date(time).getFullYear()} год уже наступил`
+  }
+}
+
+isNY('2024-01-01');
