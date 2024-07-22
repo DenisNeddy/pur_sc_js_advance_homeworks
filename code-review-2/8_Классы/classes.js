7b004b056d85ea481db639b88a01d3b2e5fceed2
Denis <fordz2012@gmail.com>
2024-07-22 17:47:26 +0300

    corrected task 8_classes

--- code-review-2/8_Классы/classes.js
+++ code-review-2/8_Классы/classes.js
@@ -1,23 +1,68 @@
+// class Car {
+//   #model;
+//   #make;
+//   #run;
+
+//   constructor(model,make,run) {
+//     this.#model = model;
+//     this.#make = make;
+//     this.#run = run;
+//   }
+
+//   get run() {
+//     return this.#run;
+//   }
+
+//   set changeRun (value) {
+//     this.#run = value;
+//   }
+
+//   info() {
+//     console.log(`Марка авто - ${this.#model}; Год выпуска - ${this.#make}; Пробег - ${this.#run};`);
+//   }
+// }
+
 class Car {
-  #model;
-  #make;
-  #run;
+  #_brand;
+  #_model;
+  #_mileage;
 
-  constructor(model,make,run) {
-    this.#model = model;
-    this.#make = make;
-    this.#run = run;
+  constructor(brand, model, mileage) {
+    this.#_brand = brand;
+    this.#_model = model;
+    this.#mileage = mileage;
   }
 
-  get run() {
-    return this.#run;
+  get #mileage() {
+    return this.#_mileage;
   }
 
-  set changeRun (value) {
-    this.#run = value;
+  set #mileage(value) {
+    this.#_mileage = value;
+  }
+
+  #checkMileage(value) {
+    return value > this.#mileage;
+  }
+
+  changeMileage(value) {
+    if(!this.#checkMileage(value)) {
+      console.error(
+        `
+        the values are the same (current: ${this.#mileage}, new: ${value}). Mileage NOT changed
+        `
+      );
+      return false;
+    }
+
+    console.log(`mileage changed from ${this.#mileage} to ${value}`);
+    this.#_mileage = value;
+    return true;
   }
 
   info() {
-    console.log(`Марка авто - ${this.#model}; Год выпуска - ${this.#make}; Пробег - ${this.#run};`);
+    console.log(
+      `Марка: ;${this.#_brand}, Модель: ${this.#_model}, Пробег: ${this.#mileage}`
+    )
   }
 }
